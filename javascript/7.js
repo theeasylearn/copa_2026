d61c12969e27be85f6173f824e3e049aa39c50a7
theeasylearn <theeasylearn@gmail.com>
2026-06-29 15:25:37 +0530

    function and array

---
+++ javascript/7.js
@@ -0,0 +1,19 @@
+// function
+
+function sumOfTwoNum(a, b) {
+    let sum = a + b;
+    document.write('sum is: ', sum);
+    document.write('<br>');
+}
+
+sumOfTwoNum(50,60);
+sumOfTwoNum(40,30);
+sumOfTwoNum(10,20);
+
+function hello(name) {
+    document.write('Hello ', name);
+    document.write('<br>');
+}
+
+hello('Muntazir');
+hello('Ankit sir');
---
+++ javascript/8.js
@@ -0,0 +1,15 @@
+// array -> store multiple values in same variable
+// array.length -> Total length of array
+
+let marks = [400, 500, 300, 200, 100, 154, 475];
+
+let numbers = [10, 9, 5, 4];
+
+let fruits = ['Apple', 'Banana', 'Mango'];
+
+for(let i = 0; i<marks.length; i++) {
+    document.write(marks[i]);
+    document.write('<br>');
+}
+
+// marks[0] -> return 1st value at index 0
